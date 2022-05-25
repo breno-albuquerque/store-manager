b64074670c4c280d514630b789f078b0005c0d3e
Breno Albuquerque <brenoralbu@hotmail.com>
2022-05-25 16:33:26 -0300

    feat: Requisição get - all products - completa

--- models/productsModel.js
+++ models/productsModel.js
@@ -3,7 +3,7 @@ const connection = require('../db');
 async function getAll() {
   const query = 'SELECT * FROM StoreManager.products';
 
-  const [result] = connection.execute(query);
+  const [result] = await connection.execute(query);
 
   return result;
 }
