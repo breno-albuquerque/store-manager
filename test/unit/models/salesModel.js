d635f72379294a2c3ccef09b4e2736a04befd001
Breno Albuquerque <brenoralbu@hotmail.com>
2022-05-26 15:33:30 -0300

    test: salesModel getById

--- test/unit/models/salesModel.js
+++ test/unit/models/salesModel.js
@@ -4,14 +4,14 @@ const sinon = require('sinon');
 const salesModel = require('../../../models/salesModel');
 const connection = require('../../../db');
 
-const saleExample1 = {
+const completeSale1 = {
   saleId: 1,
   date: "2021-09-09T04:54:29.000Z",
   productId: 1,
   quantity: 2
 }
 
-const saleExample2 = {
+const completeSale2 = {
   saleId: 1,
   date: "2021-09-09T04:54:54.000Z",
   productId: 2,
@@ -20,7 +20,7 @@ const saleExample2 = {
 
 describe('Busca todas vendas no model', () => {
   before(async () => {
-    const dataMock = [[saleExample1]];
+    const dataMock = [[completeSale1, completeSale2]];
 
     sinon.stub(connection, 'execute').resolves(dataMock);
   });
@@ -40,6 +40,39 @@ describe('Busca todas vendas no model', () => {
       const result = await salesModel.getAll();
 
       expect(result[0]).to.include.all.keys('saleId', 'date', 'productId', 'quantity');
+    });
+  });
+});
+
+describe('Busca venda por Id no model', () => {
+  const saleExample1 = {
+    id: 1,
+    date: "2021-09-09T04:54:54.000Z"
+  }
+
+  before(async () => {
+    const dataMock = [[saleExample1]];
+
+    sinon.stub(connection, 'execute').resolves(dataMock);
+  });
+
+  after(async () => {
+    connection.execute.restore();
+  });
+
+  describe('Em caso de sucesso', () => {
+    it('Retorna um array com um único objeto', async () => {
+      const result = await salesModel.getById(1);
+
+      expect(result).to.be.an('array');
+      expect(result).to.have.length(1);
+      expect(result[0]).to.be.an('object');
+    });
+    it('O objeto possui o id correto', async () => {
+      const result = await salesModel.getById(1);
+
+      expect(result[0]).to.have.property('id');
+      expect(result[0].id).to.equal(1);
     });
   });
 });
