1f8001194c8c338590696cdefafebadc521d6a9c
Breno Albuquerque <brenoralbu@hotmail.com>
2022-05-26 16:17:04 -0300

    test: salesService getSalesById

--- test/unit/services/salesService.js
+++ test/unit/services/salesService.js
@@ -8,21 +8,20 @@ const salesModel = require('../../../models/salesModel');
 
 const { expect } = chai;
 
-const completeSale1 = {
-  sale_id: 1,
-  date: "2021-09-09T04:54:29.000Z",
-  product_id: 1,
-  quantity: 2
-}
-
-const completeSale2 = {
-  sale_id: 1,
-  date: "2021-09-09T04:54:54.000Z",
-  product_id: 2,
-  quantity: 2
-}
-
 describe('Busca todas vendas no service', () => {
+  const completeSale1 = {
+    sale_id: 1,
+    date: "2021-09-09T04:54:29.000Z",
+    product_id: 1,
+    quantity: 2
+  }
+  
+  const completeSale2 = {
+    sale_id: 1,
+    date: "2021-09-09T04:54:54.000Z",
+    product_id: 2,
+    quantity: 2
+  }
   before(async () => {
     const modelMock = [completeSale1, completeSale2];
 
@@ -42,18 +41,53 @@ describe('Busca todas vendas no service', () => {
   });
 });
 
-/* describe('Busca venda por id no service', () => {
+describe('Busca venda por id no service', () => {
+  const saleExample1 = {
+    id: 1,
+    date: "2021-09-09T04:54:54.000Z"
+  }
+
+  const saleProductExample1 = {
+    sale_id: 1,
+    product_id: 1,
+    quantity: 5
+  }
+
+  
   describe('Em caso de id válido', () => {
-    it('Retorna um array de objetos', () => {
-
+    before(async () => {
+      sinon.stub(salesModel, 'getById').resolves([saleExample1]);
+      sinon.stub(salesModel, 'getSalesProduct').resolves([saleProductExample1]);
     });
-    it('Os objetos possuem as chaves corretas', () => {
+  
+    after(async () => {
+      salesModel.getById.restore();
+      salesModel.getSalesProduct.restore();
+    });
 
+    it('Retorna um array de objetos', async () => {
+      const result = await salesService.getSalesById(1);
+
+      expect(result).to.be.an('array');
+      expect(result[0]).to.be.an('object');
+    });
+    it('Os objetos possuem as chaves corretas', async () => {
+      const result = await salesService.getSalesById(1);
+
+      expect(result[0]).to.include.all.keys('date', 'productId', 'quantity');
     });
   });
+
   describe('Em caso de id inválido', () => {
-    it('Uma excessão deve ser lançada com a mensagem "Sale not found"', () => {
+    before(async () => {
+      sinon.stub(salesModel, 'getById').resolves([]);  
+    });
+    after(async () => {
+      salesModel.getById.restore();
+    });
 
+    it('Uma excessão deve ser lançada com a mensagem "Sale not found"', async () => {
+      await expect(salesService.getSalesById('Id inválido')).to.be.rejectedWith(Error, 'Sale not found');
     });
   });
-}); */
+});
