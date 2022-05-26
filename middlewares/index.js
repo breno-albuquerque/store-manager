5d695a6e19974ffc0687d3adbb018a869ff98e10
Breno Albuquerque <brenoralbu@hotmail.com>
2022-05-26 17:26:19 -0300

    feat: Adiciona middleware validateProductQuantity

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,7 +1,9 @@
 const errorMiddleware = require('./errorMiddleware');
 const validateProductName = require('./validateProductName');
+const validateProductQuantity = require('./validateProductQuantity');
 
 module.exports = {
   errorMiddleware,
   validateProductName,
+  validateProductQuantity,
 };
