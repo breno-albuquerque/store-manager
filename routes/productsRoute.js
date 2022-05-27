05120d75484eb1a0dbf73670d1b4596d0d12457d
Breno Albuquerque <brenoralbu@hotmail.com>
2022-05-26 22:34:24 -0300

    feat: Rota para atualizar um produto completa

--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -12,5 +12,6 @@ const {
 router.get('/', productController.getProducts);
 router.get('/:id', productController.getById);
 router.post('/', validateProductName, validateProductQuantity, productController.postProduct);
+router.put('/:id', validateProductName, validateProductQuantity, productController.updateProduct);
 
 module.exports = router;
