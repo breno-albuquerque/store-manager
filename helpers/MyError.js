f6c063ab4c43a23eeaecf8d613b87ff735d26295
Breno Albuquerque <brenoralbu@hotmail.com>
2022-05-30 08:20:47 -0300

    refactor: Erro personalizado agora é uma Classe

--- helpers/MyError.js
+++ helpers/MyError.js
@@ -1,10 +1,11 @@
-function MyError(message, status) {
-  this.name = 'MyError';
-  this.message = message;
-  this.status = status;
-  this.stack = (new Error()).stack;
+class MyError {
+  constructor(message, status) {
+    this.name = 'MyError';
+    this.message = message;
+    this.status = status;
+    this.stack = (new Error()).stack;
+  }
 }
 MyError.prototype = Object.create(MyError.prototype);
-MyError.prototype.constructor = MyError;
 
 module.exports = MyError;
