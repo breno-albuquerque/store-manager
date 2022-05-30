12b4ab70b6325267c3b7ffabb034ff092a214c8d
Breno Albuquerque <brenoralbu@hotmail.com>
2022-05-30 16:49:24 -0300

    fix: Remove comentários da trybe

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const middlewares = require('./middlewares');
 
 const app = express();
 
-// não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
@@ -15,7 +14,4 @@ app.use(Router);
 
 app.use(middlewares.errorMiddleware);
 
-// não remova essa exportação, é para o avaliador funcionar
-// você pode registrar suas rotas normalmente, como o exemplo acima
-// você deve usar o arquivo index.js para executar sua aplicação 
 module.exports = app;
