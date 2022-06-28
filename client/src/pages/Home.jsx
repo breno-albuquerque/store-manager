81012de80065b6f201ff4325829b4ff1fbbdc6a0
Breno Albuquerque <brenoralbu@hotmail.com>
2022-06-28 09:52:57 -0300

    feat(client): Adiciona tabela com produtos vindos do servidor

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { getProducts } from '../services/requests';
 
 function Home() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const data = await getProducts();
+      setProducts(data);
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
-    <div>Home</div>
+    <main>
+      <table>
+        <thead>
+          <tr>
+            <td>Id</td>
+            <td>Name</td>
+            <td>Quantity</td>
+          </tr>
+        </thead>
+        <tbody>
+          { products.length > 0 && products.map((product) => {
+            const { id, name, quantity } = product;
+            return (
+              <tr key={id}>
+                <td>{ id }</td>
+                <td>{ name }</td>
+                <td>{ quantity }</td>
+              </tr>
+            );
+          }) }
+        </tbody>
+      </table>
+
+    </main>
   );
 }
 
