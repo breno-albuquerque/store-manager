0626d0f8b8b0a35c82f99b7f8886d393cb7bdef2
Breno Albuquerque <brenoralbu@hotmail.com>
2022-06-28 09:30:44 -0300

    feat(client): Cria estruturas iniciais das páginas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Sales from './pages/Sales';
+
 function App() {
   return (
     <Routes>
-      <Route exact path="/" element={} />
-      <Route exact path="/products" element={} />
-      <Route exact path="/sales" element={} />
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/products" element={<Products />} />
+      <Route exact path="/sales" element={<Sales />} />
     </Routes>
   );
 }
