f4f422edefb650dcb16d0c7b5d0362ddc6a783fc
Breno Albuquerque <brenoralbu@hotmail.com>
2022-06-28 15:39:35 -0300

    feat(client): Cria estrutura do form de adicionar Vendas

--- client/src/pages/Sales.jsx
+++ client/src/pages/Sales.jsx
@@ -2,7 +2,39 @@ import React from 'react';
 
 function Sales() {
   return (
-    <div>Sales</div>
+    <form>
+      <label
+        htmlFor="product-id"
+      >
+        <input
+          onChange={}
+          name="productId"
+          value={}
+          placeholder="Product Id"
+          id="product-id"
+          type="text"
+        />
+      </label>
+
+      <label
+        htmlFor="product-quantity"
+      >
+        <input
+          onChange={}
+          value={}
+          name="quantity"
+          placeholder="Quantity"
+          id="product-quantity"
+          type="text"
+        />
+        </label>
+
+      <Button
+        type="button"
+        onClick={}
+      >
+      </Button>
+    </form>
   );
 }
 
