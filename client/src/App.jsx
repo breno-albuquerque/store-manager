adf21744bb76e47d54ee0933a547c1454a4549e1
Breno Albuquerque <brenoralbu@hotmail.com>
2022-06-28 09:28:31 -0300

    feat(client): Cria estrutura de rotas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      Hello front!
-    </div>
+    <Routes>
+      <Route exact path="/" element={} />
+      <Route exact path="/products" element={} />
+      <Route exact path="/sales" element={} />
+    </Routes>
   );
 }
 
