17397d1f49ba9b01acd42288fe610a4aad77c241
Breno Albuquerque <brenoralbu@hotmail.com>
2022-06-28 08:51:27 -0300

    refactor(server): Exclui arquivo index.js e move conteúdo para app.js

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const Router = require('./routes');
 const middlewares = require('./middlewares');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 
@@ -16,5 +17,9 @@ app.use(cors());
 app.use(Router);
 
 app.use(middlewares.errorMiddleware);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Escutando na porta ${process.env.PORT}`);
+});
 
 module.exports = app;
