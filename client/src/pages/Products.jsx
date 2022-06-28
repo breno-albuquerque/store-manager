e0e04c4c6f712287ec4f035e010934751efa22d8
Breno Albuquerque <brenoralbu@hotmail.com>
2022-06-28 10:56:58 -0300

    feat(client): Cria estrutura do form de adicionar Produto

--- client/src/pages/Products.jsx
+++ client/src/pages/Products.jsx
@@ -2,7 +2,35 @@ import React from 'react';
 
 function Products() {
   return (
-    <div>Products</div>
+    <form>
+      <label
+        htmlFor="product-name"
+      >
+        <input
+          placeholder="Name"
+          onChange={}
+          value={}
+          name="name"
+          id="product-name"
+          type="text"
+        />
+      </label>
+
+      <label
+        htmlFor="product-quantity"
+      >
+        <input
+          placeholder="Quantity"
+          onChange={}
+          value={}
+          name="quantity"
+          id="product-quantity"
+          type="number"
+        />
+      </label>
+
+      <button onClick={handleAddClick} type="button">Add</button>
+    </form>
   );
 }
 
