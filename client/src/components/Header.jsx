de8ef4a771f6e019d473fb1d2ecef09c7e7c34c8
Breno Albuquerque <brenoralbu@hotmail.com>
2022-06-29 13:43:27 -0300

    style(client): Adiciona estilo ao Header com react-bootstrap

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,38 +1,61 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import {
+  Container, Nav, Navbar,
+} from 'react-bootstrap';
+
 function Header() {
   const navigate = useNavigate();
 
-  const handleRedirect = ({ target }) => {
-    const { value } = target;
-    navigate(value);
+  const handleRedirect = (event, endpoint) => {
+    event.preventDefault();
+    navigate(endpoint);
   };
 
   return (
-    <header>
-      <button
-        value="/"
-        type="button"
-        onClick={handleRedirect}
-      >
-        Home
-      </button>
-      <button
-        value="/products"
-        type="button"
-        onClick={handleRedirect}
-      >
-        Product
-      </button>
-      <button
-        value="/sales"
-        type="button"
-        onClick={handleRedirect}
-      >
-        Sales
-      </button>
-    </header>
+    <Navbar
+      bg="dark"
+      variant="dark"
+      expand="lg"
+    >
+      <Container>
+        <Navbar.Brand
+          href="#home"
+        >
+          Navbar
+        </Navbar.Brand>
+        <Navbar.Toggle
+          aria-controls="basic-navbar-nav"
+        />
+        <Navbar.Collapse
+          id="basic-navbar-nav"
+        >
+          <Nav
+            className="me-auto"
+          >
+            <Nav.Link
+              type="button"
+              onClick={(event) => handleRedirect(event, '/')}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              type="button"
+              onClick={(event) => handleRedirect(event, '/products')}
+            >
+              Products
+            </Nav.Link>
+            <Nav.Link
+              type="button"
+              onClick={(event) => handleRedirect(event, '/sales')}
+            >
+              Sales
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
