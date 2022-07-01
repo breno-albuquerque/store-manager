fc0bc48194283db46793ba7198350f8f69952b91
Breno Albuquerque <brenoralbu@hotmail.com>
2022-07-01 08:17:53 -0300

    refactor(server): Corrige ordem de imports no app.js

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
+const cors = require('cors');
 const Router = require('./routes');
 const middlewares = require('./middlewares');
-const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
