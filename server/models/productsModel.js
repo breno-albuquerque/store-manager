4be75238834503b0f7613639c97daf26eaa40ba0
Breno Albuquerque <brenoralbu@hotmail.com>
2022-07-03 17:59:14 -0300

    fix(server): Tenta usar usar crases nas querys para resolver bug

--- server/models/productsModel.js
+++ server/models/productsModel.js
@@ -1,31 +1,32 @@
 const connection = require('../db');
+require('dotenv').config();
 
 const getAll = async () => {
-  const query = 'SELECT * FROM products';
+  const query = `SELECT * FROM ${process.env.MYSQL_DATABASE}.products`;
   const [result] = await connection.execute(query);
   return result;
 };
 
 const getById = async (id) => {
-  const query = 'SELECT * FROM products WHERE id = ?';
+  const query = `SELECT * FROM ${process.env.MYSQL_DATABASE}.products WHERE id = ?`;
   const [product] = await connection.execute(query, [id]);
   return product;
 };
 
 const postProduct = async (name, quantity) => {
-  const query = 'INSERT INTO products (name, quantity) VALUES (?, ?)';
+  const query = `INSERT INTO ${process.env.MYSQL_DATABASE}.products (name, quantity) VALUES (?, ?)`;
   const [result] = await connection.execute(query, [name, quantity]);
   return result;
 };
 
 const updateProduct = async (id, name, quantity) => {
-  const query = 'UPDATE products SET name=?, quantity=? WHERE id=?';
+  const query = `UPDATE ${process.env.MYSQL_DATABASE}.products SET name=?, quantity=? WHERE id=?`;
   const [result] = await connection.execute(query, [name, quantity, id]);
   return result;
 };
 
 const deleteProduct = async (id) => {
-  const query = 'DELETE FROM products WHERE id = ?';
+  const query = `DELETE ${process.env.MYSQL_DATABASE}.FROM products WHERE id = ?`;
   const [result] = await connection.execute(query, [id]);
   return result;
 };
--- server/models/salesModel.js
+++ server/models/salesModel.js
@@ -1,45 +1,39 @@
 const connection = require('../db');
+require('dotenv').config();
 
 const getAllSales = async () => {
-  const query = `
-  SELECT sl.quantity, s.date, sl.sale_id, sl.product_id, p.name
-  FROM StoreManager.sales_products AS sl
-  INNER JOIN sales AS s
-  ON s.id = sl.sale_id
-  INNER JOIN products AS p
-  ON p.id = sl.product_id
-  `;
+  const query = 'SELECT sl.quantity, s.date, sl.`sale_id`, sl.`product_id`, p.name FROM' + process.env.MYSQL_DATABASE + '.`sales_products` AS sl INNER JOIN sales AS s ON s.id = sl.`sale_id` INNER JOIN products AS p ON p.id = sl.product_id';
   const [result] = await connection.execute(query);
   return result;
 };
 
 const getSaleById = async (id) => {
-  const query = 'SELECT * FROM StoreManager.sales WHERE id = ?';
+  const query = `SELECT * FROM ${process.env.MYSQL_DATABASE}.sales WHERE id = ?`;
   const [result] = await connection.execute(query, [id]);
   return result;
 };
 
 const getSalesProduct = async (id) => {
-  const query = 'SELECT * FROM StoreManager.sales_products WHERE sale_id = ?';
+  const query = `SELECT * FROM ${process.env.MYSQL_DATABASE}.sales_products WHERE sale_id = ?`;
   const [result] = await connection.execute(query, [id]);
   return result;
 };
 
 const postSales = async (date) => {
-  const query = 'INSERT INTO StoreManager.sales (date) VALUES (?)';
+  const query = `INSERT INTO ${process.env.MYSQL_DATABASE}.sales (date) VALUES (?)`;
   const [result] = await connection.execute(query, [date]);
   return result;
 };
 
 const postSalesProduct = async (productId, saleId, quantity) => {
-  const query = `INSERT INTO StoreManager.sales_products (product_id, sale_id, quantity) 
+  const query = `INSERT INTO ${process.env.MYSQL_DATABASE}.sales_products (product_id, sale_id, quantity) 
   VALUES (?, ?, ?)`;
   const [result] = await connection.execute(query, [productId, saleId, quantity]);
   return result;
 };
 
 const updateSalesProduct = async (saleId, productId, quantity) => {
-  const query = `UPDATE StoreManager.sales_products SET product_id=?, quantity=?
+  const query = `UPDATE ${process.env.MYSQL_DATABASE}.sales_products SET product_id=?, quantity=?
   WHERE sale_id=? AND product_id=?`;
   const [result] = await connection.execute(query, [productId, quantity, saleId, productId]);
 
@@ -47,13 +41,13 @@ const updateSalesProduct = async (saleId, productId, quantity) => {
 };
 
 const deleteSale = async (id) => {
-  const query = 'DELETE FROM StoreManager.sales WHERE id = ?';
+  const query = `DELETE FROM ${process.env.MYSQL_DATABASE}.sales WHERE id = ?`;
   const [result] = await connection.execute(query, [id]);
   return result;
 };
 
 const deleteSalesProduct = async (id) => {
-  const query = 'DELETE FROM StoreManager.sales_products WHERE sale_id = ?';
+  const query = `DELETE FROM ${process.env.MYSQL_DATABASE}.sales_products WHERE sale_id = ?`;
   const [result] = await connection.execute(query, [id]);
   return result;
 };
