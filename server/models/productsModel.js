5434ca446d3c763e932626e35916e5410946494e
Breno Albuquerque <brenoralbu@hotmail.com>
2022-07-03 17:37:10 -0300

    fix(server): Corrige querys das camadas models para usar variáveis de ambiente

--- server/models/productsModel.js
+++ server/models/productsModel.js
@@ -1,31 +1,31 @@
 const connection = require('../db');
 
 const getAll = async () => {
-  const query = 'SELECT * FROM StoreManager.products';
+  const query = 'SELECT * FROM products';
   const [result] = await connection.execute(query);
   return result;
 };
 
 const getById = async (id) => {
-  const query = 'SELECT * FROM StoreManager.products WHERE id = ?';
+  const query = 'SELECT * FROM products WHERE id = ?';
   const [product] = await connection.execute(query, [id]);
   return product;
 };
 
 const postProduct = async (name, quantity) => {
-  const query = 'INSERT INTO StoreManager.products (name, quantity) VALUES (?, ?)';
+  const query = 'INSERT INTO products (name, quantity) VALUES (?, ?)';
   const [result] = await connection.execute(query, [name, quantity]);
   return result;
 };
 
 const updateProduct = async (id, name, quantity) => {
-  const query = 'UPDATE StoreManager.products SET name=?, quantity=? WHERE id=?';
+  const query = 'UPDATE products SET name=?, quantity=? WHERE id=?';
   const [result] = await connection.execute(query, [name, quantity, id]);
   return result;
 };
 
 const deleteProduct = async (id) => {
-  const query = 'DELETE FROM StoreManager.products WHERE id = ?';
+  const query = 'DELETE FROM products WHERE id = ?';
   const [result] = await connection.execute(query, [id]);
   return result;
 };
